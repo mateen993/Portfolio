a9be1f7c81b10a15d7e2c3d1cc02fea783a02fd9
Abdul Mateen <mateenabdul993@gmail.com>
2022-07-06 18:05:42 +0500

    Added title and favicon

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import About from './Components/About';
 import Portfolio from './Components/Portfolio';
 import Experience from './Components/Experience';
 import Contact from './Components/Contact';
+import {Helmet} from 'react-helmet'
 
 function App() {
   return (
     <div>
+      <Helmet>
+            <meta charSet="utf-8" />
+            <title>My Portfolio Website</title>
+            <link rel="canonical" href="https://my-portolio.netlify.app/" />
+            <meta name="description" content="This is my portfolio website I made with the use of React and Tailwind css" />
+      </Helmet>
       <Navbar />
       <div className='pt-20'>
       <Home />
